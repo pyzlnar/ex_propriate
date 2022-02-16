d3eff7c07155e9ae90d890c1db3ac1e734426ec6
pyzlnar@Megumin <pyzlnar@gmail.com>
2022-02-16 09:54:05 -0600

    :tada: Initial Commit

---
+++ lib/ex_propriate.ex
@@ -0,0 +1,18 @@
+defmodule ExPropriate do
+  @moduledoc """
+  Documentation for `ExPropriate`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExPropriate.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_propriate_test.exs
@@ -0,0 +1,8 @@
+defmodule ExPropriateTest do
+  use ExUnit.Case
+  doctest ExPropriate
+
+  test "greets the world" do
+    assert ExPropriate.hello() == :world
+  end
+end
